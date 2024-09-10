167f3a1e136cb2a76b27c150c5f3e502cbf05bda
Kornach Daniil <danik.kornach@gmail.com>
2024-09-10 12:09:28 +0300

    Создал по шаблону новый предмет(монета), которую игрок сможет поднять

--- Source/SuperSideScroller/PickableActor_Base.h
+++ Source/SuperSideScroller/PickableActor_Base.h
@@ -22,7 +22,7 @@ public:
 	void BeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor
 		, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	void PlayerPickedUp(ASuperSidescroller_Player* Player);
+	virtual void PlayerPickedUp(ASuperSidescroller_Player* Player);
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	USoundBase* PickupSound;
